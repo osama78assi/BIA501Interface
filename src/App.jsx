f296adc39c2bca671ca7fb0ab2d0014b5cad500b
osama78assi <osama78assi@gmail.com>
2024-06-17 23:46:02 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
 import { Provider } from "react-redux";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import { HashLoader } from "react-spinners";
 import Header from "./components/header/Header";
+import Form from "./pages/Form";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import store from "./store";
-const Form = lazy(() => import("./pages/Form"));
 
 function App() {
   return (
@@ -16,19 +16,11 @@ function App() {
         <div className="relative overflow-y-hidden overflow-x-hidden h-[100%]">
           <Header />
           <div className="applayout">
-            <Suspense
-              fallback={
-                <div className="w-full h-screen bg-white top-0 bottom-0 absolute flex justify-center items-center ">
-                  <HashLoader className="custom-animation" />
-                </div>
-              }
-            >
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="*" element={<PageNotFound />} />
-              </Routes>
-            </Suspense>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
           </div>
         </div>
       </BrowserRouter>
