549547bc8b6d2b4ba346695760cf573cfbc008a6
osama78assi <osama78assi@gmail.com>
2024-11-28 14:42:11 +0400

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="mt-20 flex flex-col justify-center items-center gap-6 h-full">
       <h1 className="font-bold text-center text-3xl">
-        This Assignment Website Has Been the Cumulative Work of:
+        This Assignment Website Has Been done by:
       </h1>
 
       <Card className="w-9/12 py-10 min-h-96 flex justify-center items-center">
@@ -16,30 +16,6 @@ function Home() {
               <h3 className="text-center font-bold">Osama Assi</h3>
               <h3 className="text-center font-bold">osama_167541</h3>
             </Card>
-          </div>
-          <div className="bg-blue-400 flex justify-center items-center rounded-lg w-1/3 p-4">
-            <Card className="w-11/12 py-4">
-              <h3 className="text-center font-bold">Adnan Ali Al-As&apos;ad</h3>
-              <h3 className="text-center font-bold">adnan_189704</h3>
-            </Card>{" "}
-          </div>
-          <div className="bg-blue-400 flex justify-center items-center rounded-lg w-1/3 p-4">
-            <Card className="w-11/12 py-4">
-              <h3 className="text-center font-bold">Aisha Shamoot</h3>
-              <h3 className="text-center font-bold">aisha_shamoot_159220</h3>
-            </Card>{" "}
-          </div>
-          <div className="bg-blue-400 flex justify-center items-center rounded-lg w-1/3 p-4">
-            <Card className="w-11/12 py-4">
-              <h3 className="text-center font-bold">Laila Hussam Said</h3>
-              <h3 className="text-center font-bold">laila_204265</h3>
-            </Card>{" "}
-          </div>
-          <div className="bg-blue-400 flex justify-center items-center rounded-lg w-1/3 p-4">
-            <Card className="w-11/12 py-4">
-              <h3 className="text-center font-bold">Raghad Kamal Alden</h3>
-              <h3 className="text-center font-bold">raghad_120170</h3>
-            </Card>{" "}
           </div>
         </div>
       </Card>
